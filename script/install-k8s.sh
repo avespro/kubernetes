5df46ec29185ddec66fe775829aff2964cd2aa5b
avespro <126699904+avespro@users.noreply.github.com>
2023-03-01 18:11:04 +0100

    Update install-k8s.sh

--- script/install-k8s.sh
+++ script/install-k8s.sh
@@ -3,6 +3,8 @@ K8S_VERSION="1.25.6"
 RUNC_VERSION="1.1.4"
 CONTAINERD_VERSION="1.6.16"
 CLI_ARCH="amd64"
+apt update
+apt install -y apt-transport-https ca-certificates curl
 # Installing and configure prerequisites
 # Disabling swap
 sed -i 's/\/swap/#\/swap/' /etc/fstab
