d05229e84514110f25a44697705865ab3d460d11
avespro <126699904+avespro@users.noreply.github.com>
2023-03-15 10:57:31 +0100

    Update install-k8s.sh

--- script/install-k8s.sh
+++ script/install-k8s.sh
@@ -5,8 +5,7 @@ RUNC_VERSION="1.1.4"
 CONTAINERD_VERSION="1.6.16"
 CLI_ARCH="amd64"
 
- apt-get update 
- apt-get upgrade
+ sudo apt update && sudo apt upgrade
  sudo apt install -y apt-transport-https ca-certificates curl
 
 # Installing and configure prerequisites
